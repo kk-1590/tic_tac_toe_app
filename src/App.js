181f098583f69d293a0f60911422ab4473c24f8b
kk-1590 <kkhandelwal1590@gmail.com>
2023-11-06 23:44:54 +0530

    initialized the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board></Board>
+      
     </div>
   );
+}
+
+function Board(){
+  const [marks,setMarks] = useState([0,0,0,0,0,0,0,0,0]);
+
+  const changeMark = () => {
+
+  }
+
+    return (
+        <div className='Board'>
+          <div>
+            <Block mark={marks[0]}></Block>
+            <Block mark={marks[1]}></Block>
+            <Block mark={marks[2]}></Block>
+          </div>
+          <div>
+            <Block mark={marks[3]}></Block>
+            <Block mark={marks[4]}></Block>
+            <Block mark={marks[5]}></Block>
+          </div>
+          <div>
+            <Block mark={marks[6]}></Block>
+            <Block mark={marks[7]}></Block>
+            <Block mark={marks[8]}></Block>
+          </div>            
+        </div>
+    )
+}
+
+function Block({mark,changeMark}){
+  return (
+    <div className={`Block ${mark}`} onClick={(e) => changeMark}>
+      
+    </div>
+)
 }
 
 export default App;
